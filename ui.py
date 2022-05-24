56fa16c900a8e71adfc2f751b38874d38b7946b7
Anna <anna_tsigip@mail.ru>
2022-05-24 21:16:18 +0300

    ui

--- ui.py
+++ ui.py
@@ -1,36 +1,11 @@
-#1 введение данных
+def hello():
+    print('Добрый день.')
+    result = int(input("Выберите операцию: \n \
+        1 - добавить контакт \n \
+        2 - найти контакт \n \
+        3 - показать весь справочник \n "))
+    return result
 
-def first_name():
-    first_name = input("Введите фамилию: ").capitalize()
-    return first_name
 
-def last_name():
-    last_name = input("Введите имя: ").capitalize()
-    return last_name
-
-def phone_number():
-    phone_number = input("Введите номер телефона: ").capitalize()
-    return phone_number
-
-def additional_data():
-    additional_data = input("Дополнительные данные: ").capitalize()
-    return additional_data
-
-with open('file.txt', 'a', encoding='utf-8') as data:
-    data.write(f'Фамилия: {first_name()}\n')
-    data.write(f'Имя: {last_name()}\n')
-    data.write(f'Номер: {phone_number()}\n')
-    data.write(f'Описание: {additional_data()}\n')
-
-# with open('file.csv', 'a', encoding='utf-8') as data:
-#     data.write(f'Фамилия: {first_name()}\n')
-#     data.write(f'Имя: {last_name()}\n')
-#     data.write(f'Номер: {phone_number()}\n')
-#     data.write(f'Описание: {additional_data()}\n')
-
-# чтение 
-# with open('file.txt', 'r', encoding='utf-8') as data:
-#     for i in data:
-#         string = data.readline()
-#         print(string)
+#print(Hello())
 
