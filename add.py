d202a33477175eb1b55af91a450e5ad469097a8d
Anna <anna_tsigip@mail.ru>
2022-05-25 20:21:18 +0300

    controller

--- add.py
+++ add.py
@@ -1,4 +1,4 @@
-def add():
+def add(added):
     added = []
     first_name = input("Введите фамилию: ").capitalize()
     added.append(first_name)
@@ -8,31 +8,13 @@ def add():
     added.append(phone_number)
     additional_data = input("Дополнительные данные: ").capitalize()
     added.append(additional_data)
-    print('Добавлен контакт: ')
+    print('Добавлен контакт: added')
     return added
 #print(f'{first_name};{last_name};{phone_number};{dditional_data}') 
-print(add())
+#print(add())
 
 
 
-
-# with open('file.txt', 'a', encoding='utf-8') as data:
-#     data.write(f'Фамилия: {first_name()}\n')
-#     data.write(f'Имя: {last_name()}\n')
-#     data.write(f'Номер: {phone_number()}\n')
-#     data.write(f'Описание: {additional_data()}\n')
-
-# with open('file.csv', 'a', encoding='utf-8') as data:
-#     data.write(f'Фамилия: {first_name()}\n')
-#     data.write(f'Имя: {last_name()}\n')
-#     data.write(f'Номер: {phone_number()}\n')
-#     data.write(f'Описание: {additional_data()}\n')
-
-# чтение 
-# with open('file.txt', 'r', encoding='utf-8') as data:
-#     for i in data:
-#         string = data.readline()
-#         print(string)
 
 
 
--- controller.py
+++ controller.py
@@ -1,21 +1,21 @@
 import ui
 import add
+import logger
 
 def run():
     do_it = ui.hello()
     if do_it == 1:
         print('Давайте внесем данные')
+        added = add.added() # внесение контакта
+        logger.log(added)
+    # if do_it == 2:
+    #     print('Кого ищем?')
 
-        do = add.added() # внесение контакта
-        log.log_to_file(do)
-    if do_it == 2:
-        print('Кого ищем?')
+    #     do = find.find() # поиск
+    #     log.reading_file(do)
 
-        do = find.find() # поиск
-        log.reading_file(do)
-
-    if do_it == 3:
-        print('Справочник: ') 
-        log.read_all_file()
+    # if do_it == 2:
+    #     print('Справочник: ') 
+    #     log.read_all_file()
 
 
